78495cf727d87cb0d2a6f1ef74a24f1e1af3336c
Abigail <abi_cruz_12@hotmail.com>
2016-08-11 20:53:01 -0400

    email fix more

--- js/contact_me.js
+++ js/contact_me.js
@@ -50,7 +50,7 @@ $(function() {
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to sales@abbydistributor.com<a href='sales@abbydistributor.com?Subject=Message_Me from abbyps.com;>sales@abbydistributor.com</a> ? Sorry for the inconvenience!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + " it seems that my mail server is not responding...</strong> Could you please email me directly to <a href='sales@abbydistributor.com'>sales@abbydistributor.com</a>? Sorry for the inconvenience!");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
